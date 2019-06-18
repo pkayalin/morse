c39b21b03a9115f2f075103a89e9a6521a0ce5cb
unknown <paulkayal@yahoo.fr>
2019-06-18 16:23:12 +0200

    Morse code. Failing tests. Pending Jenkins configuration

---
+++ morse.py
@@ -0,0 +1,5 @@
+class Morse:
+    def decode(self, message):
+        pass # TODO: implement the behavior!
+        morse = Morse()
+        sentence = morse.decode(".- .-.. .-.. / -.-- --- ..- / -. . . -.. / .. ... / -.-. --- -.. .")
